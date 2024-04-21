e14e4f27a2c06af25a47fa44ab5571e416a8b10d
Ivan <109656369+altabib@users.noreply.github.com>
2024-04-21 15:13:51 +0300

    HW-1

---
+++ src/generics/3.ts
@@ -0,0 +1,3 @@
+function merge<T extends {}, U extends {}>(objA: T, objB: U):(T & U) {
+      return Object.assign({}, objA, objB);
+    }
---
+++ src/generics/5.ts
@@ -0,0 +1,16 @@
+export enum UserRole {
+      admin = 'admin',
+      editor = 'editor',
+      guest = 'guest',
+    }
+    
+    type UserRoleDescription = Record<UserRole, string>
+
+    const RoleDescription: UserRoleDescription = {
+      [UserRole.admin]: 'Admin User',
+      [UserRole.editor]: 'Editor User',
+      [UserRole.guest]: 'Guest User',
+    };
+    
+    
+    RoleDescription
